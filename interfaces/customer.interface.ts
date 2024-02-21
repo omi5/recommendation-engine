051f3000b674773f27f7b3faebd4cca7392f61d2
Tanveer Chowdhury <ct.anzoom@gmail.com>
2024-02-21 22:09:06 +0600

    fix: modified interfaces to make others optional

--- interfaces/customer.interface.ts
+++ interfaces/customer.interface.ts
@@ -2,19 +2,19 @@
 import { ICustomerPreference } from "./CustomerPreference.interface";
 import { ILongLat } from "./longLat.interface";
 export interface ICustomer {
-    _id?: string;
-    name: string;
-    email: string;
-    dob: Date;
+    _id: string; //needed
+    name: string; //needed
+    email?: string;
+    dob?: Date;
     age?: number;
     customerImage?: string;
-    phoneNumber: string;
-    password: string;
-    address: string;
-    currentLatLong: ILongLat;
+    phoneNumber?: string;
+    password?: string;
+    address?: string;
+    currentLatLong: ILongLat; //needed
     doorwayLatLong?: ILongLat;
     uprn?: string;
-    allOrderIdList: string[];
-    customerPreference: ICustomerPreference; // Tasty Tag Enums from Menu and category
+    allOrderIdList?: string[];
+    customerPreference: ICustomerPreference; // //needed //Tasty Tag Enums from Menu and category
     loyaltyPoints?: number;
   } 
